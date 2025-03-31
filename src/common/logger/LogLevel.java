bd4eb5ce765d88807be581d0edcd8b104c0341ad
KlnSdr <monsieurk1209@gmail.com>
2025-03-31 17:28:49 +0200

    initial commit

---
+++ src/common/logger/LogLevel.java
@@ -0,0 +1,14 @@
+package common.logger;
+
+public enum LogLevel {
+    ERROR(Colors.RED), WARN(Colors.YELLOW), INFO(Colors.GREEN), DEBUG(Colors.BLUE);
+
+    private final Colors color;
+    LogLevel(Colors color) {
+        this.color = color;
+    }
+
+    public String getColorized() {
+        return color.getColor() + this.name() + Colors.RESET.getColor();
+    }
+}
